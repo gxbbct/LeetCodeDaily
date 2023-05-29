ccfd3c323c44eddcf9e21d8c99d775fa457f0c08
gxbbct <974863147@qq.com>
2023-05-29 11:38:41 +0800

    20230529

---
+++ 3/3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> occ;
+        int n = s.size();
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                occ.erase(s[i - 1]);
+            }
+            while (rk + 1 < n && !occ.count(s[rk + 1])) {
+                occ.insert(s[rk + 1]);
+                ++rk;
+            }
+            ans = max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    string mys;
+    cin >> mys;
+    cout << s.lengthOfLongestSubstring(mys) << endl;
+}
